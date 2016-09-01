1c9a4ef6f65ab21c91c01635cd4ec45bfce1aa74
touktw <touktw@gmail.com>
2016-09-01 12:24:09 +0900

    3.6.3 get() 에 queryForObject 사용 RowMapper 는 한개의 결과를 가져올 것을 예상 ResultSet.next() 호출 후 mapRow() 호출

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -5,6 +5,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import springbook.user.domain.User;
@@ -35,32 +36,47 @@ public class UserDao {
         user.getId(), user.getName(), user.getPassword());
   }
 
-  public User get(String id) throws ClassNotFoundException, SQLException {
-    Connection c = dataSource.getConnection();
-
-    PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-    ps.setString(1, id);
-
-    ResultSet rs = ps.executeQuery();
-
-    User user = null;
-    if (rs.next()) {
-      user = new User();
-      user.setId(rs.getString("id"));
-      user.setName(rs.getString("name"));
-      user.setPassword(rs.getString("password"));
-    }
-
-    rs.close();
-    ps.close();
-    c.close();
-
-    if (user == null) {
-      throw new EmptyResultDataAccessException(1);
-    }
-
-    return user;
+  public User get(String id) {
+    return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id},
+        new RowMapper<User>() {
+          @Override
+          public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            // this method call after call next()
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+          }
+        });
   }
+
+//  public User get(String id) throws ClassNotFoundException, SQLException {
+//    Connection c = dataSource.getConnection();
+//
+//    PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+//    ps.setString(1, id);
+//
+//    ResultSet rs = ps.executeQuery();
+//
+//    User user = null;
+//    if (rs.next()) {
+//      user = new User();
+//      user.setId(rs.getString("id"));
+//      user.setName(rs.getString("name"));
+//      user.setPassword(rs.getString("password"));
+//    }
+//
+//    rs.close();
+//    ps.close();
+//    c.close();
+//
+//    if (user == null) {
+//      throw new EmptyResultDataAccessException(1);
+//    }
+//
+//    return user;
+//  }
 
   public void deleteAll() throws SQLException {
     this.jdbcTemplate.update("delete from users");
