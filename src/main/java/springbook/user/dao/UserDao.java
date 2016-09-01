17203fbe7ec5e4d8fc448f1628f4de4793d7eea0
touktw <touktw@gmail.com>
2016-09-01 10:52:07 +0900

    3.5.2

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -66,10 +66,14 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
+        executeSql("delete from users");
+    }
+
+    private void executeSql(final String query) throws SQLException {
         this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
             @Override
             public PreparedStatement makePrepareStatement(Connection c) throws SQLException {
-                return c.prepareStatement("delete from users");
+                return c.prepareStatement(query);
             }
         });
     }
