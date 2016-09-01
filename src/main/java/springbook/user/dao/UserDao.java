d78d90e85363e003c5520c915ee8db8cd83cd915
touktw <touktw@gmail.com>
2016-09-01 12:39:22 +0900

    3.6.5 재사용 가능한 RowMapper 분리 코드 정리

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -19,8 +19,17 @@ import java.util.List;
  * Created by tae.kim on 2016. 8. 29..
  */
 public class UserDao {
-  private DataSource dataSource;
   private JdbcTemplate jdbcTemplate;
+  private RowMapper<User> userMapper = new RowMapper<User>() {
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+      User user = new User();
+      user.setId(rs.getString("id"));
+      user.setName(rs.getString("name"));
+      user.setPassword(rs.getString("password"));
+      return user;
+    }
+  };
 
   public UserDao() {
   }
@@ -28,8 +37,6 @@ public class UserDao {
   public void setDataSource(DataSource dataSource) {
     this.jdbcTemplate = new JdbcTemplate();
     this.jdbcTemplate.setDataSource(dataSource);
-    this.dataSource = dataSource;
-
   }
 
   public void add(final User user) throws ClassNotFoundException, SQLException {
@@ -39,45 +46,8 @@ public class UserDao {
 
   public User get(String id) {
     return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id},
-        new RowMapper<User>() {
-          @Override
-          public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-            // this method call after call next()
-            User user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
-            return user;
-          }
-        });
+        this.userMapper);
   }
-
-//  public User get(String id) throws ClassNotFoundException, SQLException {
-//    Connection c = dataSource.getConnection();
-//
-//    PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-//    ps.setString(1, id);
-//
-//    ResultSet rs = ps.executeQuery();
-//
-//    User user = null;
-//    if (rs.next()) {
-//      user = new User();
-//      user.setId(rs.getString("id"));
-//      user.setName(rs.getString("name"));
-//      user.setPassword(rs.getString("password"));
-//    }
-//
-//    rs.close();
-//    ps.close();
-//    c.close();
-//
-//    if (user == null) {
-//      throw new EmptyResultDataAccessException(1);
-//    }
-//
-//    return user;
-//  }
 
   public void deleteAll() throws SQLException {
     this.jdbcTemplate.update("delete from users");
@@ -88,31 +58,7 @@ public class UserDao {
     return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
   }
 
-//  public int getCount() {
-//    return this.jdbcTemplate.query(new PreparedStatementCreator() {
-//      @Override
-//      public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//        return con.prepareStatement("select count(*) from users");
-//      }
-//    }, new ResultSetExtractor<Integer>() {
-//      @Override
-//      public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-//        rs.next();
-//        return rs.getInt(1);
-//      }
-//    });
-//  }
-
   public List<User> getAll() {
-    return this.jdbcTemplate.query("select * from users order by id", new RowMapper<User>() {
-      @Override
-      public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
-        return user;
-      }
-    });
+    return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
   }
 }
