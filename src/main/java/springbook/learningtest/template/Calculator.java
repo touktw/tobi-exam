268e57b1be03838ddf139604fd25e20e3af33d80
touktw <touktw@gmail.com>
2016-09-01 12:00:42 +0900

    3.5.3 #3 템플릿 / 콜백 재설계

--- src/main/java/springbook/learningtest/template/Calculator.java
+++ src/main/java/springbook/learningtest/template/Calculator.java
@@ -29,33 +29,47 @@ public class Calculator {
     }
   }
 
-  public Integer calcSum(String filepath) throws IOException {
-    BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
-      @Override
-      public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-        Integer sum = 0;
-        String line = null;
-        while ((line = br.readLine()) != null) {
-          sum += Integer.valueOf(line);
+  public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+    BufferedReader br = null;
+    try {
+      br = new BufferedReader(new FileReader(filepath));
+      Integer res = initVal;
+      String line = null;
+      while ((line = br.readLine()) != null) {
+        res = callback.doSomethingWithLine(line, res);
+      }
+      return res;
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      throw e;
+    } finally {
+      if (br != null) {
+        try {
+          br.close();
+        } catch (IOException e) {
+          System.out.println(e.getMessage());
         }
-        return sum;
+      }
+    }
+  }
+
+  public Integer calcSum(String filepath) throws IOException {
+    LineCallback sumCallback = new LineCallback() {
+      @Override
+      public Integer doSomethingWithLine(String line, Integer value) {
+        return value + Integer.valueOf(line);
       }
     };
-    return fileReadTemplate(filepath, sumCallback);
+    return lineReadTemplate(filepath, sumCallback, 0);
   }
 
   public Integer calcMultiply(String filepath) throws IOException {
-    BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
+    LineCallback sumCallback = new LineCallback() {
       @Override
-      public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-        Integer multiply = 1;
-        String line = null;
-        while ((line = br.readLine()) != null) {
-          multiply *= Integer.valueOf(line);
-        }
-        return multiply;
+      public Integer doSomethingWithLine(String line, Integer value) {
+        return value * Integer.valueOf(line);
       }
     };
-    return fileReadTemplate(filepath, multiplyCallback);
+    return lineReadTemplate(filepath, sumCallback, 1);
   }
 }
